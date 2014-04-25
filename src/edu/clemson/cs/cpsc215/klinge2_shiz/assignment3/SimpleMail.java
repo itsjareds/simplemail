a4e4c0364587d51bc966b634ef9bd71be0f2068a
Jared Klingenberger <klinge2+git@g.clemson.edu>
2014-04-25 02:37:09 -0400

    Added SSL authentication support.

--- src/edu/clemson/cs/cpsc215/klinge2_shiz/assignment3/SimpleMail.java
+++ src/edu/clemson/cs/cpsc215/klinge2_shiz/assignment3/SimpleMail.java
@@ -8,6 +8,11 @@ public class SimpleMail {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	    AuthenticationInfo auth = null;
+	    
+	    if (args.length >= 3)
+	        auth = new AuthenticationInfo(args[0], args[1], args[2]);
+	    
 		DataStore storage = DataStore.getInstance();
 		
 		Configuration conf = storage.loadConfig();
@@ -18,13 +23,15 @@ public class SimpleMail {
 		if (conf.getSmtpServer() == null)
 			conf.setSmtpServer("smtp.clemson.edu");
 		
-		contacts.add(new Contact("Jared Klingenberger", "address here",
+		if (contacts.size() == 0) {
+		    contacts.add(new Contact("Jared Klingenberger", "address here",
 				"555-555-5555", "klinge2@clemson.edu"));
+		}
 		
-		EmailHandler handler = new EmailHandler(conf);
+		EmailHandler handler = new EmailHandler(conf, auth);
 		
-		Email email = new Email(null, contacts, null,
-				 "cc'd", "Tried making a mailing list");
+		Email email = new Email(contacts, null, null,
+				 "simplemail message", "you have received a simplemail!");
 		
 		handler.sendMail(email);
 		
