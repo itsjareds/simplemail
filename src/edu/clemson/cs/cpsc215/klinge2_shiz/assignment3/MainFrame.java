1af08d07a26263e43c9aae694232e04b01512968
Jared Klingenberger <klinge2+git@g.clemson.edu>
2014-04-26 07:25:54 -0400

    Changd look and feel to System theme.

--- src/edu/clemson/cs/cpsc215/klinge2_shiz/assignment3/MainFrame.java
+++ src/edu/clemson/cs/cpsc215/klinge2_shiz/assignment3/MainFrame.java
@@ -31,14 +31,14 @@ public class MainFrame extends JFrame {
 
 	public static void main(String [] args) {
 		try {
-//			UIManager.setLookAndFeel(
-//					UIManager.getSystemLookAndFeelClassName());
-			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
+			UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
+//			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+//				if ("Nimbus".equals(info.getName())) {
+//					UIManager.setLookAndFeel(info.getClassName());
+//					break;
+//				}
+//			}
 		} catch (Exception e) {
 			System.out.println("Unable to modify look and feel.");
 		}
